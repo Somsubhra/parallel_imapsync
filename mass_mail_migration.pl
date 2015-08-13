19e9249e9731598000e3193c2fe832a7ae872d67
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-13 18:52:51 +0530

    Fix coding style

--- mass_mail_migration.pl
+++ mass_mail_migration.pl
@@ -29,9 +29,8 @@ use Parallel::ForkManager;
 my $pm = Parallel::ForkManager->new($num_processes);
 
 sub get_logging_time {
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
-    my $nice_timestamp = sprintf ( "%04d/%02d/%02d %02d:%02d:%02d",
-                                   $year+1900,$mon+1,$mday,$hour,$min,$sec);
+    my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
+    my $nice_timestamp = sprintf("%04d/%02d/%02d %02d:%02d:%02d", $year + 1900, $mon + 1, $mday, $hour, $min, $sec);
     return $nice_timestamp;
 }
 
