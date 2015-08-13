cf088ed70bf6c4657e20fcc57f197713bab694a9
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-13 16:55:44 +0530

    Read from csv file and pass it to script in child process

--- mailbox_sync.pl
+++ mailbox_sync.pl
@@ -3,14 +3,35 @@
 use strict;
 use warnings;
 
-use Getopt::Long qw(GetOptions);
-my $email_address;
+my %opt;
+use Getopt::Std;
+my $opt_string = 'u:p:r:o:l:a:e:dt';
+getopts( "$opt_string", \%opt ) or usage();
 
-GetOptions("e=s" => \$email_address);
+my $remote_username = $opt{u};
+my $local_username = $opt{e};
+my $remote_password = $opt{p};
+my $remote_hostname = $opt{r};
+my $remote_port = $opt{o};
+my $local_hostname = $opt{l};
+my $local_password = $opt{a};
+my $debug = $opt{d};
+my $test = $opt{t};
 
-if(!(defined($email_address))) {
-	print("Usage: mailbox_sync.pl -e <email_address>\n");
-	exit 1;
+if ((!defined($remote_username)) or (!defined($local_username)) or (!defined($remote_password)) or (!defined($remote_hostname)) or (!defined($remote_port)) or (!defined($local_hostname)) or (!defined($local_password))) {
+        print "usage: mailbox_sync.pl OPTIONS
+        -u remote username
+        -e local username
+        -p remote password
+        -a local password
+        -r remote imap host
+        -o remote imap port
+        -l local imap host
+        -d debug\n";
+
+        syslog('info',"please read usage, insufficient parameters passed");
+
+        exit 1
 }
 
-print "Running fake mailbox_sync on $email_address\n";
+print "Running fake mailbox_sync on $remote_username:$local_username:$remote_password:$remote_hostname:$remote_port:$local_hostname:$local_password\n";
--- mass_mail_migration.pl
+++ mass_mail_migration.pl
@@ -4,17 +4,35 @@ use strict;
 use warnings;
 
 use Getopt::Long qw(GetOptions);
-my $num_threads;
+my $num_processes;
 
-GetOptions('j=s' => \$num_threads);
+GetOptions('j=s' => \$num_processes);
 
-if(!(defined($num_threads))) {
-	$num_threads = 10;
-	print "Using default value for number of threads: 10\n";
+if(!(defined($num_processes))) {
+	$num_processes = 10;
+	print "Using default value for number of maximum processes: 10\n";
 }
 
-print "Number of threads chosen: $num_threads\n";
+print "Number of maximum processes chosen: $num_processes\n";
 
 use Parallel::ForkManager;
 
-my $pm = Parallel::ForkManager->new($num_threads);
+my $pm = Parallel::ForkManager->new($num_processes);
+
+open(my $fh, '<', "data.csv") or die $!;
+
+while(my $data = <$fh>) {
+	my @literals = split /,/, $data, 7;
+
+	my $remote_username = $literals[0];
+	my $local_username = $literals[1];
+	my $remote_password = $literals[2];
+	my $local_password = $literals[3];
+	my $remote_imap_host = $literals[4];
+	my $remote_imap_port = $literals[5];
+	my $local_imap_host = $literals[6];
+
+	my $pid = $pm->start and next;
+	print system("/usr/bin/perl mailbox_sync.pl -u $remote_username -e $local_username -p $remote_password -a $local_password -r $remote_imap_host -o $remote_imap_port -l $local_imap_host");
+	$pm->finish;
+}
